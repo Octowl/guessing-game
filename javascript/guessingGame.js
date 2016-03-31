cf3542be612ed2a98ad956990e455f46d54e3ebe
Octowl <alsaff1987@gmail.com>
2016-03-31 19:53:49 +0300

    Move entire script into an IIFE

--- javascript/guessingGame.js
+++ javascript/guessingGame.js
@@ -1,195 +1,198 @@
-/* **** Global Variables **** */
-// try to elminate these global variables in your project, these are here just to start.
+(function() {
+  /* **** Global Variables **** */
+  // try to elminate these global variables in your project, these are here just to start.
 
-var playersGuess,
-  winningNumber
+  var playersGuess,
+    winningNumber;
 
-var previousGuesses = [];
+  var previousGuesses = [];
 
-/* **** Prototype Additions **** */
+  /* **** Prototype Additions **** */
 
-// Shuffle the array in place
-Array.prototype.shuffle = function() {
-  var j, x, i;
-  for (i = this.length; i; i -= 1) {
-    j = Math.floor(Math.random() * i);
-    x = this[i - 1];
-    this[i - 1] = this[j];
-    this[j] = x;
-  }
-};
-
-/* **** Guessing Game Functions **** */
-
-// Generate the Winning Number
-
-function generateWinningNumber() {
-  // Returns a random number between 1 - 100
-  return Math.floor(Math.random() * 100 + 1);
-}
-
-// Fetch the Players Guess
-
-function playersGuessSubmission() {
-
-  // hide hints
-  $("#oracle").slideUp();
-
-  var guess = $("#victim-guess");
-  // TODO: add some validation here
-  playersGuess = +guess.val();
-  guess.val("");
-  checkGuess();
-}
-
-// Determine if the next guess should be a lower or higher number
-
-function lowerOrHigher() {
-  // returns the difference
-  return playersGuess - winningNumber;
-}
-
-// Construct a message letting the player know how far off they are
-
-function guessMessage() {
-  var difference = lowerOrHigher();
-  var message = {};
-
-  message.strength = "Your invocation is too " + ((difference < 0) ? "weak" : "strong") + ".";
-  difference = Math.abs(difference);
-
-  if (difference > 20) {
-    message.distance = "That was so far off it banished some other unrelated elder beast!";
-  } else if (difference > 10) {
-    message.distance = "You have banished...an innocent squid...";
-  } else if (difference > 5) {
-    message.distance = "That one was actually close!";
-  } else {
-    message.distance = "The Octowl feels fear for the first time in its existence!";
-  }
-
-  return message;
-}
-
-// Check if the Player's Guess is the winning number
-
-function checkGuess() {
-  var message;
-  if (playersGuess === winningNumber) {
-    playerWins();
-  } else {
-    message = guessMessage();
-    if ($.inArray(playersGuess, previousGuesses) == -1) {
-      previousGuesses.push(playersGuess);
-    } else {
-      message.distance = "The Octowl is amused as you repeat yourself in desperation"
+  // Shuffle the array in place
+  Array.prototype.shuffle = function() {
+    var j, x, i;
+    for (i = this.length; i; i -= 1) {
+      j = Math.floor(Math.random() * i);
+      x = this[i - 1];
+      this[i - 1] = this[j];
+      this[j] = x;
     }
-    wrongGuess(message);
+  };
+
+  /* **** Guessing Game Functions **** */
+
+  // Generate the Winning Number
+
+  function generateWinningNumber() {
+    // Returns a random number between 1 - 100
+    return Math.floor(Math.random() * 100 + 1);
   }
-}
 
-// The palyer has won the game (animation)
+  // Fetch the Players Guess
 
-function playerWins() {
-  //reset opacity
-  $('body').removeClass('level-' + previousGuesses.length)
-    .addClass('level-0');
-  modifyHeader('has been banished to the ocean depths\n' +
-    'There it shall slumber for a thousand years, ' +
-    'dreaming of a new number that will end the world');
+  function playersGuessSubmission() {
 
-}
+    // hide hints
+    $("#oracle").slideUp();
 
-// The player has lost the game (animation)
+    var guess = $("#victim-guess");
+    // TODO: add some validation here
+    playersGuess = +guess.val();
+    guess.val("");
+    checkGuess();
+  }
 
-function playerLoses() {
-  $('body').removeClass('level-' + previousGuesses.length)
-    .addClass('end-game');
-  modifyHeader('has arisen from the depths!\n' +
-    'It devours all it encounters!\n' +
-    'You have failed!\n' +
-    'The world ends!');
-}
+  // Determine if the next guess should be a lower or higher number
 
-// Modify the header text (animation)
+  function lowerOrHigher() {
+    // returns the difference
+    return playersGuess - winningNumber;
+  }
 
-function modifyHeader(message) {
-  //fadeout extra controls
-  $('h2, .guess-controls, #reset, #hint').fadeOut(600, function() {
-    //fadeout heading
-    $('h1').fadeOut(600, function() {
-      //change heading to win message
-      $(this).text('The Ancient Octowl\n' + message);
-      $(this).html($(this).html().replace(/\n/g, '<br/>'));
-      //fadein heading
-      $(this).fadeIn(600, function() {
-        //change text on play again button and fadein
-        $('#reset').text('Battle Again!').fadeIn();
+  // Construct a message letting the player know how far off they are
+
+  function guessMessage() {
+    var difference = lowerOrHigher();
+    var message = {};
+
+    message.strength = "Your invocation is too " + ((difference < 0) ? "weak" : "strong") + ".";
+    difference = Math.abs(difference);
+
+    if (difference > 20) {
+      message.distance = "That was so far off it banished some other unrelated elder beast!";
+    } else if (difference > 10) {
+      message.distance = "You have banished...an innocent squid...";
+    } else if (difference > 5) {
+      message.distance = "That one was actually close!";
+    } else {
+      message.distance = "The Octowl feels fear for the first time in its existence!";
+    }
+
+    return message;
+  }
+
+  // Check if the Player's Guess is the winning number
+
+  function checkGuess() {
+    var message;
+    if (playersGuess === winningNumber) {
+      playerWins();
+    } else {
+      message = guessMessage();
+      if ($.inArray(playersGuess, previousGuesses) == -1) {
+        previousGuesses.push(playersGuess);
+      } else {
+        message.distance = "The Octowl is amused as you repeat yourself in desperation"
+      }
+      wrongGuess(message);
+    }
+  }
+
+  // The palyer has won the game (animation)
+
+  function playerWins() {
+    //reset opacity
+    $('body').removeClass('level-' + previousGuesses.length)
+      .addClass('level-0');
+    modifyHeader('has been banished to the ocean depths\n' +
+      'There it shall slumber for a thousand years, ' +
+      'dreaming of a new number that will end the world');
+
+  }
+
+  // The player has lost the game (animation)
+
+  function playerLoses() {
+    $('body').removeClass('level-' + previousGuesses.length)
+      .addClass('end-game');
+    modifyHeader('has arisen from the depths!\n' +
+      'It devours all it encounters!\n' +
+      'You have failed!\n' +
+      'The world ends!');
+  }
+
+  // Modify the header text (animation)
+
+  function modifyHeader(message) {
+    //fadeout extra controls
+    $('h2, .guess-controls, #reset, #hint').fadeOut(600, function() {
+      //fadeout heading
+      $('h1').fadeOut(600, function() {
+        //change heading to win message
+        $(this).text('The Ancient Octowl\n' + message);
+        $(this).html($(this).html().replace(/\n/g, '<br/>'));
+        //fadein heading
+        $(this).fadeIn(600, function() {
+          //change text on play again button and fadein
+          $('#reset').text('Battle Again!').fadeIn();
+        });
       });
     });
-  });
-}
-
-// The player guessed wrong (animation)
-
-function wrongGuess(message) {
-  $('body').addClass('level-' + previousGuesses.length)
-    .removeClass('level-' + (previousGuesses.length - 1));
-  if (previousGuesses.length == 5) {
-    playerLoses();
-  } else {
-    $('#strength').fadeOut(100, function() {
-      $(this).text(message.strength).fadeIn(100);
-    });
-    $('#mockery').fadeOut(100, function() {
-      $(this).text(message.distance).fadeIn(100);
-    });
-    $('#counter').text('' + 5 - previousGuesses.length + ' More Attempts Until The Octowl Awakens');
   }
-}
 
-// Create a provide hint button that provides additional clues to the "Player"
+  // The player guessed wrong (animation)
 
-function provideHint() {
-  var numbers = [];
-  var hintCount = (5 - previousGuesses.length) * 2;
-  var newNum = winningNumber;
-  //remove previous hints
-  $('#oracle').slideUp();
-  $("li").remove();
-  //generate new hints
-  for (var i = hintCount; i; i--) {
-    while ($.inArray(newNum, previousGuesses.concat(numbers)) !== -1) {
-      newNum = generateWinningNumber();
+  function wrongGuess(message) {
+    $('body').addClass('level-' + previousGuesses.length)
+      .removeClass('level-' + (previousGuesses.length - 1));
+    if (previousGuesses.length == 5) {
+      playerLoses();
+    } else {
+      $('#strength').fadeOut(100, function() {
+        $(this).text(message.strength).fadeIn(100);
+      });
+      $('#mockery').fadeOut(100, function() {
+        $(this).text(message.distance).fadeIn(100);
+      });
+      $('#counter').text('' + 5 - previousGuesses.length + ' More Attempts Until The Octowl Awakens');
     }
-    numbers.push(newNum);
   }
-  numbers.shuffle();
-  numbers.forEach(function(number) {
-    $('#oracle').append('<li>' + number + '</li>');
-  });
-  $('#oracle').slideDown(600);
-}
 
-// Allow the "Player" to Play Again
+  // Create a provide hint button that provides additional clues to the "Player"
 
-function playAgain() {
-  document.location.reload();
-}
-
-
-/* **** Event Listeners/Handlers ****  */
-$(document).ready(function() {
-  winningNumber = generateWinningNumber();
-  $("#guess").click(playersGuessSubmission);
-  $("#victim-guess").keypress(function(e) {
-    if (e.which == 13) {
-      playersGuessSubmission();
+  function provideHint() {
+    var numbers = [];
+    var hintCount = (5 - previousGuesses.length) * 2;
+    var newNum = winningNumber;
+    //remove previous hints
+    $('#oracle').slideUp();
+    $("li").remove();
+    //generate new hints
+    for (var i = hintCount; i; i--) {
+      while ($.inArray(newNum, previousGuesses.concat(numbers)) !== -1) {
+        newNum = generateWinningNumber();
+      }
+      numbers.push(newNum);
     }
+    numbers.shuffle();
+    numbers.forEach(function(number) {
+      $('#oracle').append('<li>' + number + '</li>');
+    });
+    $('#oracle').slideDown(600);
+  }
+
+  // Allow the "Player" to Play Again
+
+  function playAgain() {
+    document.location.reload();
+  }
+
+
+  /* **** Event Listeners/Handlers ****  */
+  $(document).ready(function() {
+    winningNumber = generateWinningNumber();
+    $("#guess").click(playersGuessSubmission);
+    $("#victim-guess").keypress(function(e) {
+      if (e.which == 13) {
+        playersGuessSubmission();
+      }
+    });
+    $("#hint").click(provideHint);
+    $("#reset").click(playAgain);
+    $("#oracle").on("click", "li", function() {
+      $("#victim-guess").val($(this).text());
+    });
   });
-  $("#hint").click(provideHint);
-  $("#reset").click(playAgain);
-  $("#oracle").on("click", "li", function() {
-    $("#victim-guess").val($(this).text());
-  });
-});
+
+})();
